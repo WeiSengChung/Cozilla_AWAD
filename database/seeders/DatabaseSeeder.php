0a2be819653d8f4f709f58b65ade892e7225ff11
WeiSengChung <wschung22@hotmail.com>
2025-04-05 19:18:57 +0800

    update seeder command for better coding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
+use Database\Seeders\ProductSpecificSeeder;
+use Database\Seeders\UserProfileSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(ProductSeeder::class);
+        $this->call(ProductSpecificSeeder::class);
+        $this->call(UserProfileSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
