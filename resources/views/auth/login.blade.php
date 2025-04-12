a1b13c62b9be6ff09e9967491ace18cc54cd1e76
Xyingg0611 <xyingg2003@gmail.com>
2025-04-13 00:12:09 +0800

    resource/view/layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cozilla Log In</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
 
-<img src = "{{asset('images/Cozilla.jpg')}}" alt = "Cozilla Logo" >
 @extends('layouts.app')
 
 @section('content')
@@ -7,16 +12,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class = 'loginTitle'>
+                    <div class="card-header">{{ __('Login') }}</div>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="form-group text-center">
+                            <label for="email" class="col-md-4 col-form-label text-md-start">{{ __('Email Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 mx-auto">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -27,10 +34,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="form-group text-center">
+                            <label for="password" class="col-md-4 col-form-label text-md-start">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 mx-auto">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -54,16 +61,24 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                            <div class="loginButtonContainer">
+                                <div class="loginButton">
+                                    <button type="submit" class="login-Button">
+                                        {{ __('Login') }}
+                                    </button>
+                                </div>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                <div class="forgetPassword-Container">
+                                    <div class="auth-links">
+                                        @if (Route::has('password.request'))
+                                            <a class="btn-btn-link" href="{{ route('password.request') }}">
+                                                {{ __('Forgot Password') }}
+                                            </a>
+                                            <span>|</span>
+                                            <a href="{{ route('register') }}" class="btn-btn-link">Register</a>
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,15 +17,19 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
+        <img src="{{ asset('images/Cozilla.jpg') }}" alt="Cozilla Logo" class="logo">
+        <main class="py-4">
+            @yield('content')
+        </main>
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="logo-container">
+    </div>
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -37,30 +41,25 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="login&register">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                            @php
+                                $currentRoute = Route::currentRouteName();
+                            @endphp
+                        @endguest
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
+                        @auth
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -69,15 +68,11 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        @endauth
                     </ul>
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
 </body>
 </html>
