abc2ba765708d368dc3d63ef194f507cefa3114c
WeiSengChung <wschung22@hotmail.com>
2025-04-13 21:25:18 +0800

    ???

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -39,7 +39,7 @@
                                         {{ str_replace('_', ' ', $searchProduct->clothes_category) }}
                                     </div>
                                 </div>
-                                <div class="search-result-price">RM{{ number_format($searchProduct->price, 2) }}</div>
+                                <div class="search-result-price">RM blablabla{{ number_format($searchProduct->price, 2) }}</div>
                             </a>
                         @endforeach
                     @else
