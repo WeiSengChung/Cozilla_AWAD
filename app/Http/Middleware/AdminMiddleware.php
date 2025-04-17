fa2e7381a5a70b57ed2cb2f22d57ec97a554d008
WeiSengChung <wschung22@hotmail.com>
2025-04-17 19:02:15 +0800

    added authorization in AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Gate;
 
 class AdminMiddleware
 {
@@ -17,7 +18,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role === 'admin') {
+        if (Gate::allows('isAdmin')) {
             return $next($request);
         }
         return redirect('/login/admin')->with('error', 'You are not authorized to access this page.');
